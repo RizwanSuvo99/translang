2dbb39b506664eb874177903b7c83ef9866f76d5
Md.Rizwan Uddin <45879960+RizwanSuvo99@users.noreply.github.com>
2021-06-20 01:46:14 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,3 +4,15 @@ let dropdown = document.querySelector(".hero-navbar-list");
 menu.addEventListener("click", function () {
   dropdown.classList.toggle("display-block");
 });
+
+let featureBox = document.querySelectorAll(".features-items-box");
+let featureBoxIcon = document.querySelectorAll(".features-items-box-icon");
+for (let i = 0; i < featureBox.length; i++) {
+  let imgNumber = i + 1;
+  featureBox[i].addEventListener("mouseenter", function () {
+    featureBoxIcon[i].src = "images/feature-box-" + imgNumber + "-white.png";
+  });
+  featureBox[i].addEventListener("mouseleave", function () {
+    featureBoxIcon[i].src = "images/feature-box-" + imgNumber + "-grad.png";
+  });
+}
